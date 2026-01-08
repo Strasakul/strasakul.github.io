3b4ed7f31607f668475a7236f58d556c5cb24299
Strasakul <2felix2001@gmx.de>
2026-01-08 23:32:05 +0100

    Updates

---
+++ ts/main.js
@@ -0,0 +1,10 @@
+import { createHeader } from "./header.js";
+import { createFooter } from "./footer.js";
+import { initThemeDropdown } from "./themeDropdown.js";
+function main() {
+    createHeader();
+    createFooter();
+    initThemeDropdown();
+}
+window.addEventListener("DOMContentLoaded", main);
+//# sourceMappingURL=main.js.map
