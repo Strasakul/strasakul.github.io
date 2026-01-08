4706526f258e44812694df13ecb862442cdf2ff2
Strasakul <2felix2001@gmx.de>
2026-01-08 23:03:04 +0100

    Updates

---
+++ ts/header.js
@@ -0,0 +1,21 @@
+export function createHeader() {
+    const header = document.createElement("header");
+    header.id = "site-header";
+    header.innerHTML = `
+    <nav>
+      <div class="logo">GAMER_OS</div>
+      <div class="menu">
+        <div class="dropdown">
+          <button class="btn">Select Theme</button>
+          <div class="dropdown-content" id="themeSelect">
+            <a class="theme-item" data-theme="cyber">Cyber</a>
+            <a class="theme-item" data-theme="fantasy">Fantasy</a>
+            <a class="theme-item" data-theme="retro">Retro</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    `;
+    document.body.prepend(header);
+}
+//# sourceMappingURL=header.js.map
--- ts/main.js
+++ ts/main.js
@@ -1,22 +1,10 @@
-function getCurrentPage() {
-    var _a;
-    return (_a = window.location.pathname.split("/").pop()) !== null && _a !== void 0 ? _a : "";
+import { createHeader } from "./header.js";
+import { createFooter } from "./footer.js";
+import { initThemeDropdown } from "./themeDropdown.js";
+function main() {
+    createHeader();
+    createFooter();
+    initThemeDropdown();
 }
-function createNav() {
-    var navRoot = document.getElementById("nav-root");
-    if (!navRoot)
-        return;
-    navRoot.innerHTML = "\n     <select id=\"pageSelect\">\n        <option value=\"\">Got to...</option>\n        <option value=\"index.html\">Home</option>\n        <option value=\"second.html\">Second Page</option>\n     </select>\n    ";
-    var pageSelect = document.getElementById("pageSelect");
-    var currentPage = getCurrentPage();
-    if (pageSelect) {
-        pageSelect.value = currentPage;
-        pageSelect.addEventListener("change", function () {
-            if (pageSelect.value) {
-                window.location.href = pageSelect.value;
-            }
-        });
-    }
-}
-createNav();
+window.addEventListener("DOMContentLoaded", main);
 //# sourceMappingURL=main.js.map
